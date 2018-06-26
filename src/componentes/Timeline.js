fed0b7814f73b53a6e9d7e4953e7ae6d1938f687
Maurício Ackermann <mauricio@agenciaflin.com.br>
2018-06-26 19:13:44 -0300

    Aula 2 - Redux e Thunk

--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -1,66 +1,68 @@
 import React, { Component } from 'react';
 import FotoItem from './Foto';
 import ReactCSSTransitionGroup from 'react/lib/ReactCSSTransitionGroup';
+import TimelineApi from '../logicas/TimelineApi';
 
 export default class Timeline extends Component {
 
-    constructor(props){
-      super(props);
-      this.state = {fotos:[]};
-      this.login = this.props.login;
+  constructor(props){
+    super(props);
+    this.state = {fotos:[]};
+    this.login = this.props.login;
+  }
+
+  componentWillMount(){
+    this.props.store.subscribe(() => {
+      this.setState({fotos:this.props.store.getState()});
+    })
+  }
+
+  carregaFotos(){
+    let urlPerfil;
+    
+    if(this.login === undefined) {
+      urlPerfil = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
+    } else {
+      urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
     }
 
-    componentWillMount(){
-      this.props.store.subscribe(fotos => {
-        this.setState({fotos});
-      })
+    this.props.store.dispatch(TimelineApi.lista(urlPerfil));
+  }
+
+  componentDidMount(){
+    this.carregaFotos();
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.login !== undefined){
+      this.login = nextProps.login;
+    }else{
+      this.login = undefined;
     }
+    this.carregaFotos();
+  }
 
-    carregaFotos(){
-      let urlPerfil;
+  like(fotoId) {
+    this.props.store.dispatch(TimelineApi.like(fotoId));
+  }
 
-      if(this.login === undefined) {
-        urlPerfil = `http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`;
-      } else {
-        urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
-      }
-      this.props.store.lista(urlPerfil);
-    }
+  comenta(fotoId,textoComentario) {
+    this.props.store.dispatch(TimelineApi.comenta(fotoId, textoComentario));
+  }
 
-    componentDidMount(){
-      this.carregaFotos();
-    }
-
-    componentWillReceiveProps(nextProps){
-      if(nextProps.login !== undefined){
-        this.login = nextProps.login;
-      }else{
-        this.login = undefined;
-      }
-      this.carregaFotos();
-    }
-
-    like(fotoId) {
-      this.props.store.like(fotoId);
-    }
-
-    comenta(fotoId,textoComentario) {
-      this.props.store.comenta(fotoId,textoComentario);
-    }
-
-    render(){
-        return (
-        <div className="fotos container">
+  render(){
+    return (
+      <div className="fotos container">
         <ReactCSSTransitionGroup
           transitionName="timeline"
           transitionEnterTimeout={500}
           transitionLeaveTimeout={300}>
-            {
-              this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta.bind(this)}/>)
-            }
+          {
+            this.state.fotos.map(foto => <FotoItem key={foto.id} foto={foto} like={this.like.bind(this)} comenta={this.comenta.bind(this)}/>)
+          }
         </ReactCSSTransitionGroup>
 
-        </div>
-        );
-    }
+      </div>
+    );
+  }
 }
